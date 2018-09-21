172ef78897b32e28494e53463e33707980b3d5f3
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2018-09-21 18:08:25 +0900

    20180920 - FirstWork

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './Movie';
+
+
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://imgc.allpostersimages.com/img/print/u-g-F4S5W20.jpg?w=304&h=450"
+  },
+  {
+    title:"Full Metal Jacket",
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+  },
+  {
+    title:"Oldboy",
+    poster: "http://cfs2.tistory.com/upload_control/download.blog?fhandle=YmxvZzU1NDI5QGZzMi50aXN0b3J5LmNvbTovYXR0YWNoLzAvMTIwMDAwMDAwMDAwLmpwZw%3D%3D"
+  },
+  {
+    title:"Star Wars",
+    poster: "https://ae01.alicdn.com/kf/HTB1yIBiIpmWBuNjSspdq6zugXXau/DIY-frame-Star-Wars-Episode-I-The-Phantom-Menace-Movie-Posters-print-fabric-Silk-Print-art.jpg_640x640.jpg"
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {movies.map(movie =>{
+          return <Movie title={movie.title} poster={movie.poster} />
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
